2d8794daaf39005689f2ee306aaecbd092fcbb1d
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-23 13:22:00 +0530

    HangMan Game

---
+++ C Programs/hangMan game/clrcsr.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+void main()
+{
+   printf("Press any key to clear the screen.\n");
+ 
+   getch();
+ system("cls");
+ 
+   printf("This appears after clearing the screen.\n");
+   printf("Press any key to exit...\n");
+ 
+   getch();
+  
+}
