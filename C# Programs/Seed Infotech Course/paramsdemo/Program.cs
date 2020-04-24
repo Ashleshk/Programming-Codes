fb63a98c55e9f7f6f0bfea64ad208bc11c385a11
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-24 14:58:36 +0530

    Completed day 01 Upload project Structure

---
+++ C# Programs/Seed Infotech Course/paramsdemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace paramsdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lath1 ac = new lath1();
+            ac.fun(5,5);
+            ac.fun();
+            ac.fun(6,42,6,54,46,346,376,346,2357,5);
+        }
+    }
+}
---
+++ C# Programs/Seed Infotech Course/paramsdemo/lath1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace paramsdemo
+{
+    class lath1
+    {
+        public void fun(params int[] arr)
+        {
+            Console.WriteLine("param are used" );
+        }
+    }
+}
