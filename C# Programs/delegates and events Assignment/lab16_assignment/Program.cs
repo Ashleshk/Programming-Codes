fe7a474727b5cf73aefee94f94f88d80cf2ccff4
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-24 15:11:01 +0530

    Assignment of Events and Delegates

---
+++ C# Programs/delegates and events Assignment/lab16_assignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab16_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch;
+            accounts acc = new accounts(102015,"ashlesh",5000);
+            acc.balancezero += new del1(acc_balancezero);
+            acc.cantremove += new del1(acc_cantremove);
+            acc.underbalance += new del1(acc_underbalance);
+           do{
+            Console.WriteLine("do you want to deposit(0) or withdraw(1):");
+            ch = Convert.ToInt32(Console.ReadLine());
+            if (ch == 0)
+                acc.Deposit();
+            else
+                acc.withdraw();
+        }while(ch <2);
+           }
+        static void acc_underbalance()
+        {
+            Console.WriteLine("sorry ..... your balnce is below 1000/-");
+        }
+
+        static void acc_cantremove()
+        {
+            Console.WriteLine("ERROR can't withdraw monry.......balnce less than withdraw");
+        }
+
+        static void acc_balancezero()
+        {
+            Console.WriteLine("your balnce is ZEROOOOOO......");
+        }
+    }
+}
---
+++ C# Programs/delegates and events Assignment/lab16_assignment/accounts.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab16_assignment
+{  
+     public delegate void del1();
+    class accounts
+    {
+        public event del1 underbalance;
+        public event del1 balancezero;
+        public event del1 cantremove;
+
+        public int acc_num;
+        public string acc_name;
+        public int bal;
+        public accounts(int num,string name,int b)
+        {
+          acc_num=num;
+          acc_name=name;
+         bal=b;
+         }
+        public void withdraw()
+        {
+            Console.WriteLine("enter amount to withdraw:");
+            int wid = Convert.ToInt32(Console.ReadLine());
+            if (wid > bal)
+            {
+                cantremove();
+            }
+            else if (wid == bal)
+            {
+                balancezero();
+            }
+            else if (bal <= 1000)
+            {
+                underbalance();
+            }
+            else
+                bal = bal - wid;
+        }
+        public void Deposit()
+        {
+            Console.WriteLine("enter money to deposit:");
+            int deposit = Convert.ToInt32(Console.ReadLine());
+            bal = bal + deposit;
+        }
+
+    }
+}
