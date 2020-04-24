37cfbf5476ce0952222dcd6856484956d44a8287
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-24 14:57:24 +0530

    Method Overloading

---
+++ C# Programs/Seed Infotech Course/method overloading/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace method_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lmath obj = new lmath();
+            obj.add(5,4);
+            obj.add(2.3,7);
+
+                
+        }
+    }
+}
---
+++ C# Programs/Seed Infotech Course/method overloading/lmath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace method_overloading
+{
+    class lmath
+    {
+        public void add(int x,int y)
+        {
+            Console.WriteLine(x+y);
+        }
+        public void add(int x, double y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public void add(double x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public void add(double x, double y)
+        {
+            Console.WriteLine(x + y);
+        }
+    }
+}
