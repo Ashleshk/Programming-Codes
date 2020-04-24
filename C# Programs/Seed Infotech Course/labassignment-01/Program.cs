114f1f7d644c8a4ae2c80edabddf41b159ed0a6c
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-24 14:56:06 +0530

    Empolyee Class function Wrtiting

---
+++ C# Programs/Seed Infotech Course/labassignment-01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace labassignment_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employee ac = new employee();
+            Console.WriteLine("for manager:");
+            ac.salry(45,234,34);
+            ac.salry(34,34);
+            ac.salry(63576);
+            int b=Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("gfswrgasfh"+b);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# Programs/Seed Infotech Course/labassignment-01/employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace labassignment_01
+{
+    class employee
+    {
+        public void salry(int x, int y,int p)
+        {
+            Console.WriteLine("salry of manager");
+            Console.WriteLine(x + y+p);
+        }
+        public void salry(int x )
+        {
+            Console.WriteLine("salry of pa");
+            Console.WriteLine(x );
+        }
+        public void salry(double x, int y)
+        {
+            Console.WriteLine("salry of administarator");
+            Console.WriteLine(x + y);
+        }
+        public void salry(double x, double y)
+        {
+            Console.WriteLine("salry of president");
+            Console.WriteLine(x + y);
+        }
+    }
+}
+ 
+ 
+        
+ 
