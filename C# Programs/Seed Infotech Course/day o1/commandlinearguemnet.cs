59f25e31013379c58d85c5f1f470bb6cf2d0d7f3
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-24 14:55:28 +0530

    Command Line Arguments

---
+++ C# Programs/Seed Infotech Course/day o1/commandlinearguemnet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace day_o1
+{
+    class commandlinearguemnet
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length;i++ )
+
+                Console.WriteLine(args[i]);
+        }
+         
+    }
+}
