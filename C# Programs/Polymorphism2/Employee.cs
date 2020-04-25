36ae87a2c5ec8055acc88bfe14fa7dde9719e184
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-25 14:18:06 +0530

    Polymorphsim Concept on Employee Problem

---
+++ C# Programs/Polymorphism2/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Polymorphism2
+{
+    class Employee
+    {
+        public int id;
+        public string name;
+        public int basicSalary;
+
+        public Employee()
+        {
+            id = 1;
+            name = "Aditya";
+            basicSalary = 1000;
+        }
+        public Employee(string n,int i,int s)
+        {
+            id = i;
+            name = n;
+            basicSalary = s;
+        }
+
+        public  virtual void calculateSalary()
+        { 
+        }
+    }
+}
---
+++ C# Programs/polymorphism/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            circle ob = new circle();
+            ob.draw();
+            Console.WriteLine(ob.ToString() );          
+                
+        }
+    }
+    class shape
+    {
+        public virtual  void draw()
+        {
+            Console.WriteLine("draw method of the shape.....");
+        }
+        public override string ToString()
+        {
+            return "you are in shape...,,,,,l.,.";
+        }
+        
+    }
+    class circle : shape 
+    {
+        public override void draw()
+        { Console.WriteLine("draw method of circle class.........."); 
+        }
+
+        public override string ToString()
+        {
+            return "dhaamaaaal";
+        } 
+    }
+}
