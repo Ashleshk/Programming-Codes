803a5ef35f15bf84bfc1a64f699a506f10570c61
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-25 14:25:14 +0530

    User Defined Generics

---
+++ C# Programs/UserDefinedGenericClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserDefinedGenericClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myStack<int> ms = new myStack<int>(5);
+            ms.push(30);
+            ms.push(20);
+            ms.push(10);
+            ms.pop();
+            myStack<string> ms1 = new myStack<string>(3);
+            ms1.push("Ad");
+            ms1.push("it");
+            ms1.push("ya");
+            ms1.pop();
+        }
+    }
+}
---
+++ C# Programs/UserDefinedGenericClass/myStack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserDefinedGenericClass
+{
+    class myStack<T>
+    {
+        T[] arr;
+        int top;
+        public myStack(int size)
+        {
+            arr = new T[size];
+            top = -1;
+        }
+        public void push( T param)
+        {
+            arr[++top] = param;
+        }
+        public void pop()
+        {
+            Console.WriteLine(arr[top--]);
+        }
+    }
+}
