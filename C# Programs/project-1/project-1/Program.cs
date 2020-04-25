1f1705a05bc3239ea3b69a4517d58bae6d9a75d7
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-25 14:35:12 +0530

    Delegates Summarised part-2

---
+++ C# Programs/project-1/project-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b;
+            b = false;
+            Console.WriteLine("b is " + b);
+            b = true;
+            Console.WriteLine("b is " + b);
+            // A bool value can control the if statement.
+            if (b) Console.WriteLine("This is executed.");
+            b = false;
+            if (b) Console.WriteLine("This is not executed.");
+            // Outcome of a relational operator is a bool value.
+            Console.WriteLine("10 > 9 is " + (10 > 9));
+        }
+    }
+}
---
+++ C# Programs/project-1/writelinedemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace writelinedemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("the writepart -1"+543);
+            Console.WriteLine("here is your oreder 10/3:"+10.0/3.0);
+            Console.WriteLine("the feb {0} or {1}:",28,30);
+
+            Console.WriteLine("Value\tSquared\tCubed");
+            for (i = 1; i < 10; i++)
+                Console.WriteLine("{0}\t{1}\t{2}", i, i * i, i * i * i);
+
+            Console.WriteLine("Here is 10/3: {0:#.##}", 10.0 / 3.0);
+        }
+    }
+}
