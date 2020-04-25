557e005089bd3868d1fc65d260107f59f96226cd
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-25 14:16:01 +0530

    ForEach demo

---
+++ C# Programs/foreachdemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace foreachdemo
+{
+    class Program
+    {
+        static void Main( )
+        {
+            int sum = 0;
+            int[] nums=new int[10];
+            for (int i = 0; i < 10; i++)
+                nums[i] = i;
+
+            foreach (int x in nums)
+            {
+                Console.WriteLine("value is: "+x);
+                sum += x;
+            }
+            Console.WriteLine("summation is :"+sum);
+        }
+    }
+}
