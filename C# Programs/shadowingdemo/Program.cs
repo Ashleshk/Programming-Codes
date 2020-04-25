bde502a89597821406b3a402c3ee140fbf638a5c
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-25 14:23:16 +0530

    Shadowing In C#

---
+++ C# Programs/shadowingdemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace shadowingdemo
+{
+    class Program
+    {
+        static void Main( )
+        {
+            sample2 ob = new sample2();
+            Console.WriteLine(ob.i);
+        }
+    }
+    class sample
+    {
+        public int i = 20;
+    }
+    class sample2 : sample 
+    {
+        public   int i = 30;                   //putting new makes shadowing concwept to appear
+    }
+      // no error exist if new is present or not ,,,, check for warning ........
+
+}
