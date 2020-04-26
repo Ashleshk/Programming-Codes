915a64fed0e670fa4512011450a995e82ba6fef2
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-26 13:27:03 +0530

    ViewModel Concept in WPF

---
+++ C# WPF/viewmodel/viewmodel/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿ 
+using System.Windows;
+ 
+
+namespace viewmodel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new DirectoryStructureViewModel();
+        }
+    }
+}
