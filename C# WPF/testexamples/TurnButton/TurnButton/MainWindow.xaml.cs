17d097e3ffefc33df0eebbfbcd873e0eae1fcb27
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-27 12:45:48 +0530

    TurnButton Example

---
+++ C# WPF/testexamples/TurnButton/TurnButton/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TurnButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VM vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new VM();
+            this.DataContext = vm;
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            vm.AngleValue = 90; 
+        }
+
+    }
+}
---
+++ C# WPF/testexamples/TurnButton/TurnButton/viewmodel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnButton
+{
+    class VM : INotifyPropertyChanged
+    {
+        private int angleValue;
+        public int AngleValue
+        {
+            get
+            {
+                return this.angleValue;
+            }
+            set
+            {
+                this.angleValue = value;
+                RaisePropertyChanged("AngleValue");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
