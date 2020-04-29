0e892334c0b63f6023d2280c118b566aa97543e2
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-29 13:36:04 +0530

    Calculator & Event handling

---
+++ JAVA part-2/java_home_2/src/java_home_2/ExampleEventHandling.java
@@ -0,0 +1,58 @@
+package java_home_2;
+
+ 
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class ExampleEventHandling extends Applet implements MouseListener {
+   StringBuffer strBuffer;
+
+   public void init() {
+      addMouseListener(this);
+      strBuffer = new StringBuffer();
+      addItem("initializing the apple ");
+   }
+
+   public void start() {
+      addItem("starting the applet ");
+   }
+
+   public void stop() {
+      addItem("stopping the applet ");
+   }
+
+   public void destroy() {
+      addItem("unloading the applet");
+   }
+
+   void addItem(String word) {
+      System.out.println(word);
+      strBuffer.append(word);
+      repaint();
+   }
+
+   public void paint(Graphics g) {
+      // Draw a Rectangle around the applet's display area.
+      g.drawRect(0, 0, 
+      getWidth() - 1,
+      getHeight() - 1);
+
+      // display the string inside the rectangle.
+      g.drawString(strBuffer.toString(), 10, 20);
+   }
+
+   
+   public void mouseEntered(MouseEvent event) {
+   }
+   public void mouseExited(MouseEvent event) {
+   }
+   public void mousePressed(MouseEvent event) {
+   }
+   public void mouseReleased(MouseEvent event) {
+   }
+   public void mouseClicked(MouseEvent event) {
+      addItem("mouse clicked! ");
+   }
+}
