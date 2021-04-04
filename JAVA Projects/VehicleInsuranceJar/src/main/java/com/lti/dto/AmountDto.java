3452d8ccafb841487d380a3642c159437ce01e2b
Ashleshk <ashleshuk@gmail.com>
2021-04-04 22:48:59 +0530

    Create AmountDto.java

---
+++ JAVA Projects/VehicleInsuranceJar/src/main/java/com/lti/dto/AmountDto.java
@@ -0,0 +1,14 @@
+package com.lti.dto;
+
+public class AmountDto {
+	double amount;
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+}
