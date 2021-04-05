4321214c929ef4d66cd79aeaaf5d8f753756cab1
Ashleshk <ashleshuk@gmail.com>
2021-04-05 12:51:47 +0530

    Create AdminDto.java

---
+++ JAVA Projects/VehicleInsuranceJar/src/main/java/com/lti/dto/AdminDto.java
@@ -0,0 +1,35 @@
+package com.lti.dto;
+
+public class AdminDto {
+
+int adminId;
+	
+	String adminName;
+	String password;
+	String email;
+	public int getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+	public String getAdminName() {
+		return adminName;
+	}
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
