063bd7782028d6ee6da569a1569e9f417d183736
Ashleshk <ashleshuk@gmail.com>
2021-04-07 14:18:38 +0530

    Create OtpDto.java

---
+++ JAVA Projects/VehicleInsuranceJar/src/main/java/com/lti/dto/OtpDto.java
@@ -0,0 +1,19 @@
+package com.lti.dto;
+
+public class OtpDto {
+	String email;
+	String otp;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getOtp() {
+		return otp;
+	}
+	public void setOtp(String otp) {
+		this.otp = otp;
+	}
+	
+}
