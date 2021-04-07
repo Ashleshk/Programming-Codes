58719e66abe577488d5272ba0394649045137f08
Ashleshk <ashleshuk@gmail.com>
2021-04-07 14:18:43 +0530

    Create PolicyDto.java

---
+++ JAVA Projects/VehicleInsuranceJar/src/main/java/com/lti/dto/PolicyDto.java
@@ -0,0 +1,94 @@
+package com.lti.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class PolicyDto {
+	String planType;
+	int year;
+	double amount;
+	String vehicleType;
+	String vehicleModel;
+	String manufacturer;
+	String purchaseDate; 
+	String drivingLicense;
+	String registrationNumber;
+	String engineNumber;
+	String chasisNumber;
+	String  rcDocument;
+	public String getPlanType() {
+		return planType;
+	}
+	public void setPlanType(String planType) {
+		this.planType = planType;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getVehicleType() {
+		return vehicleType;
+	}
+	public void setVehicleType(String vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+	
+	public String getVehicleModel() {
+		return vehicleModel;
+	}
+	public void setVehicleModel(String vehicleModel) {
+		this.vehicleModel = vehicleModel;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public String getPurchaseDate() {
+		return purchaseDate;
+	}
+	public void setPurchaseDate(String purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+	public String getDrivingLicense() {
+		return drivingLicense;
+	}
+	public void setDrivingLicense(String drivingLicense) {
+		this.drivingLicense = drivingLicense;
+	}
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+	public String getEngineNumber() {
+		return engineNumber;
+	}
+	public void setEngineNumber(String engineNumber) {
+		this.engineNumber = engineNumber;
+	}
+	public String getChasisNumber() {
+		return chasisNumber;
+	}
+	public void setChasisNumber(String chasisNumber) {
+		this.chasisNumber = chasisNumber;
+	}
+	public String getRcDocument() {
+		return rcDocument;
+	}
+	public void setRcDocument(String rcDocument) {
+		this.rcDocument = rcDocument;
+	}
+	
+	
+}
