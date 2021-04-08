4333f81dcd0fdda5dcd4b3597dde94bf05f9a4c3
Ashleshk <ashleshuk@gmail.com>
2021-04-08 15:52:04 +0530

    Create VehicleDetailsDto.java

---
+++ JAVA Projects/VehicleInsuranceJar/src/main/java/com/lti/dto/VehicleDetailsDto.java
@@ -0,0 +1,35 @@
+package com.lti.dto;
+
+public class VehicleDetailsDto {
+	String name;
+	
+	String type;
+	
+	double value;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+	
+	
+}
