6110ae997022ed41800835e85b42d678fe7ebe75
Ashleshk <ashleshuk@gmail.com>
2021-04-09 15:15:21 +0530

    Create Admin.java

---
+++ JAVA Projects/VehicleInsuranceJar/src/main/java/com/lti/model/Admin.java
@@ -0,0 +1,46 @@
+package com.lti.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_admin")
+public class Admin {
+
+	@Id
+	@SequenceGenerator(name="seq_admin",initialValue=30000,allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="seq_admin")
+	int adminId;
+	
+	String adminName;
+	String password;
+	String email;
+	public int getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+	public String getAdminName() {
+		return adminName;
+	}
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
