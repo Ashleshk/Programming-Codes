8896f2124bc351aae69b697c0a040be8e6d860a0
Ashleshk <ashleshuk@gmail.com>
2021-04-11 21:40:07 +0530

    Create VehicleInsuranceJarApplicationTests.java

---
+++ JAVA Projects/VehicleInsuranceJar/src/test/java/com/lti/insurance/VehicleInsuranceJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleInsuranceJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
